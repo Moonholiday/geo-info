c590dde031799f83045e04fcce799f703fc87754
moonholiday <727moonholiday@gmail.com>
2023-01-07 13:10:43 +0545

    initial nav

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return <nav className="nav">
+    <a href="/" className="site">Nepal</a>
+    <ul>
+      <li>
+        <a href="/province1"> Province1</a>
+    </li>
+    <li>
+        <a href="/province2"> Province2</a>
+      </li>
+    </ul>
+  </nav>
+}
